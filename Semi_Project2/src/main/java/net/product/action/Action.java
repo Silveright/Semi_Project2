2c799867d857142890d5ce4dcff9933796693876
DESKTOP-H23M4VO\kxksn <only-fancy@naver.com>
2022-08-09 17:12:20 +0900

    페이지 링크부분 처리

--- Semi_Project2/src/main/java/net/product/action/Action.java
+++ Semi_Project2/src/main/java/net/product/action/Action.java
@@ -6,8 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.mypage.action.ActionForward;
-
 public interface Action {
 	public ActionForward execute(HttpServletRequest request,
 								HttpServletResponse response) throws ServletException, IOException;
