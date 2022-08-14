e7aa2240887bb3b667f8c8755b089cf56d1730ca
DESKTOP-H23M4VO\kxksn <only-fancy@naver.com>
2022-08-15 02:25:05 +0900

    wishlist 추가(미완)

--- Semi_Project2/src/main/java/net/product/db/WishlistBean.java
+++ Semi_Project2/src/main/java/net/product/db/WishlistBean.java
@@ -4,9 +4,23 @@ public class WishlistBean {
 	private int wishlist_code;
 	private String id;
 	private int product_code;
+	private String opt_color;
+	private String opt_size;
 	
 	public int getWishlist_code() {
 		return wishlist_code;
+	}
+	public String getOpt_color() {
+		return opt_color;
+	}
+	public void setOpt_color(String opt_color) {
+		this.opt_color = opt_color;
+	}
+	public String getOpt_size() {
+		return opt_size;
+	}
+	public void setOpt_size(String opt_size) {
+		this.opt_size = opt_size;
 	}
 	public void setWishlist_code(int wishlist_code) {
 		this.wishlist_code = wishlist_code;
